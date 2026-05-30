01f644f593ee4400cf232fc3014173e1a1dafc24
Yzabella <yzabellamiranda4@gmail.com>
2026-05-30 19:09:41 -0300

    feat: json com as musicas do album fancy that. arquivo conection que vai retirar esses dados e por num array para ser utilizado pelo algoritmo

---
+++ backend/conection.js
@@ -0,0 +1,21 @@
+let albumData = null;
+
+// carrega o array com as musicas listadas do album no json
+async function loadAlbum() {
+    const response = await fetch('../backend/ranking.json');
+    albumData = await responde.json();
+
+    return albumData;
+}
+
+// embaralha a ordem "correta" do rank
+function shuffle(z) {
+    const array = { ...z };
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
---
+++ backend/main.js
@@ -0,0 +1,12 @@
+const userRank = [];
+
+// carrega as musicas
+async function init() {
+    const data = await loadAlbum;
+    renderAvaliable(data.tracks);
+}
+
+// depois faço
+function renderAvaliable(tracks) {
+    return
+}
