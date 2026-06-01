064fa55f15065838e3385427a3bbd35f4ff27da4
Yzabella <yzabellamiranda4@gmail.com>
2026-06-01 17:58:27 -0300

    fix: remoção de um arquivo inutilizado

--- backend/conection.js
+++
@@ -1,9 +0,0 @@
-let albumData = null;
-
-// carrega o array com as musicas listadas do album no json
-async function loadAlbum() {
-    const response = await fetch('../backend/ranking.json');
-    albumData = await response.json();
-
-    return albumData;
-}
